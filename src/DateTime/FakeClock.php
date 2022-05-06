1e72018a8829d50536aa0668704499a8c4aa2049
Karolina <karolina@example.com>
2022-05-06 21:08:13 +0200

    Basic store configuration

---
+++ src/DateTime/FakeClock.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DateTime;
+
+class FakeClock implements ClockInterface
+{
+    public function isNight(): bool
+    {
+        return false;
+    }
+}
